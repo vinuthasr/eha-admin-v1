ab4b78f3eb2a14fa6f3ac639f7ad9083809be6b5
vinuthasr <48244815+vinuthasr@users.noreply.github.com>
2019-07-19 22:09:10 +0530

    Update common.ts

--- src/common.ts
+++ src/common.ts
@@ -1,4 +1,4 @@
 //export const BaseURL = "https://ehaenterprise.herokuapp.com";
-export const BaseURL = "http://localhost:8085";
-
+//export const BaseURL = "http://localhost:8085";
+export const BaseURL = "http://ec2-13-233-74-170.ap-south-1.compute.amazonaws.com:8080";
 //  export const BaseURL = "https://ehaenterprise.herokuapp.com";  
